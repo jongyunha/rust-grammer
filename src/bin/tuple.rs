c4422aec0b5a52e65bcd9c42686ea3dbbcffce85
jongyunha <popawaw@naver.com>
2023-01-01 21:37:47 +0900

    tuple

---
+++ src/bin/tuple.rs
@@ -0,0 +1,14 @@
+enum Access {
+    Full,
+}
+
+fn one_two_three() -> (i32, i32, i32) {
+    (1, 2, 3)
+}
+
+fn main() {
+    let numbers = one_two_three();
+    let (x, y, z) = one_two_three();
+
+    let (employee, access) = ("Jake", Access::Full);
+}
